0f795ea772dd15f2d0997835b66d0021694fddbb
Axda <axda.web@gmail.com>
2022-07-11 18:07:58 +0200

    Solve [Bug report] - Login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -41,8 +41,18 @@ export default class Login {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+
+      /*  ============================= [Bug report] - Login ======================================= */
+
+      // Before
+      // email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
+      // password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
+
+      /*  ============================= [Bug report] - Login END ======================================= */
+      
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
